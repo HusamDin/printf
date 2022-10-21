4eb612e28e3de85ca9783a285182dd1d96032bdf
HusamDin <hossamn069@gmail.com>
2022-10-21 16:51:04 -0700

    BRB

--- _printf.c
+++ _printf.c
@@ -7,10 +7,13 @@ void print_buffer(char buffer[], int *buff_ind);
 
 
 /**
+
  * _printf - Printf function
+
  * @format: format.
- *
+
  * Return: Printed chars.
+
  */
 
 int _printf(const char *format, ...)
@@ -104,11 +107,13 @@ int _printf(const char *format, ...)
 
 
 /**
+
  * print_buffer - Prints the contents of the buffer if it exist
+
  * @buffer: Array of chars
+
  * @buff_ind: Index at which to add next char, represents the length.
- *
- * Return: void
+
  */
 
 void print_buffer(char buffer[], int *buff_ind)
